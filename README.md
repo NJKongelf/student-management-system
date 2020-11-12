744322ba45050c619faedf5bce69807806575820
Luis <lfgn84@gmail.com>
2020-11-12 18:39:16 +0100

    created README.md

---
+++ README.md
@@ -0,0 +1,4 @@
+●Jag kommer testa eran lösning med Insomnia, så skapa en README.md-fil i eratrepository, 
+där ni beskriver endpoints, parametrar och JSON-bodies ser ut.●README.md-filen 
+ska också innehålla kort beskrivning om vem som gjort vad (ifall nijobbat i grupp), 
+samt om ni stötte på problem som ni inte förstod hur man ska lösa.
--- src/main/java/se/iths/rest/verifiers/StudentVerifier.java
+++ src/main/java/se/iths/rest/verifiers/StudentVerifier.java
@@ -1,42 +1,41 @@
 package se.iths.rest.verifiers;
-
 import se.iths.entity.Student;
 import se.iths.rest.exeptions.BadFormatInputException;
 import se.iths.rest.exeptions.StudentNotFoundException;
 import se.iths.service.StudentService;
-
 import javax.ws.rs.core.Response;
 import java.util.List;
 
 public class StudentVerifier {
-    public BadFormatInputException badformatInput(){
+    public BadFormatInputException badformatInput() {
         throw new BadFormatInputException("Json object must include fields:\n {\n \"firstname\":\"value\" \n  \"lastname\":\"value\"\n  \"email\":\"value\"\n{\n");
     }
-    public void verifyStudent(Student student){
+
+    public void verifyStudent(Student student) {
         if (student.getEmail() == null || student.getFirstname() == null || student.getLastname() == null)
             badformatInput();
-        //return !(student.getEmail() == null || student.getFirstname() == null || student.getLastname() == null);
     }
-    public List<Student> list_stundentsCheck(List<Student> studentlist, String message){
+
+    public List<Student> list_stundentsCheck(List<Student> studentlist, String message) {
         List<Student> studentList = studentlist;
-        if(studentList.size()>0)
+        if (studentList.size() > 0)
             return studentList;
         else
             throw new StudentNotFoundException(message);
     }
 
-        public Response StudentExist(Student foundstudent, StudentService studentService) {
-        if(!(foundstudent == null)) {
+    public Response StudentExist(Student foundstudent, StudentService studentService) {
+        if (!(foundstudent == null)) {
             studentService.removeStudent(foundstudent.getId());
             return Response.ok().build();
-        }else
-        {
+        } else {
             throw new StudentNotFoundException("student attempting to delete is not registered");
         }
 
     }
-    public Student StudentExist(Student foundstudent,Long id){
-        if (foundstudent !=null) {
+
+    public Student StudentExist(Student foundstudent, Long id) {
+        if (foundstudent != null) {
             return foundstudent;
         } else {
             throw new StudentNotFoundException("Not student found with id " + id);
